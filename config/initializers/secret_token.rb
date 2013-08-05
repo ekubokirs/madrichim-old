3f19d7cdd0d7e0fe598f27ffabf7bf75f7f73fdf
Emilia Kubo Kirschenbaum <ekubokirs@gmail.com>
2013-08-05 10:15:00 -0700

    started setup and added basic form views

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Madrichim::Application.config.secret_key_base = '49b43175b066690d81660be4fd62953dccf87b20bd05a1a86da33b134f24c11b264bfcecb02bb4a9ccfc7f2b13487c773e28ff174ddbc309e4dcadd2c57186e6'
